abb3baab9d274e4159d511ac8559758ff26a7005
KevinZakky <kevinzakky9@gmail.com>
2024-10-20 16:07:30 +0700

    fix : config.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,15 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(
-  process.env.DB_URI,
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 sequelize
   .authenticate()
